0643a33504463e66ee22f68f360dd188ca256a67
Chris Allick <chrisallick@gmail.com>
2012-10-08 22:09:12 -0700

    clean up, added Fog for s3 support

--- app.rb
+++ app.rb
@@ -1,56 +1,72 @@
-#smm-albumart
-
 require 'sinatra'
-require 'sinatra/assetpack'
-require 'thin'
 require 'redis'
 
 $redis = Redis.new
+
 assets_version = 1
 
+allowed_video_upload_formats = [".png", ".gif", ".jpeg", ".jpg"]
 
 ##########   /*     */   ##########
 
-
-class App < Sinatra::Base
-  set :root, File.dirname(__FILE__)
-  register Sinatra::AssetPack
-
-  assets {
-    serve '/js',     from: 'app/public/js'        # Optional
-    serve '/css',    from: 'app/public/css'       # Optional
-    serve '/img', from: 'app/public/img'    # Optional
-
-    # The second parameter defines where the compressed version will be served.
-    # (Note: that parameter is optional, AssetPack will figure it out.)
-    js :app, '/js/jq.js', [
-      '/js/main.js',
-      ''
-    ]
-
-    css :application, '/css/main.css', [
-      '/css/reset.css'
-    ]
-
-    js_compression  :jsmin      # Optional
-  }
-
-  get '/' do
-    erb :home
-  end
-
-  App.run!({ :port => 8889 })
-
+get '/' do
+    albums = $redis.smembers( "albums" )
+    erb :home, :locals => {
+        :av => assets_version,
+        :albums => albums
+    }
 end
-
 
 # ##########   /*     */   ##########
 
 
-# class AdminHandler(BaseHandler):
-#     @tornado.web.authenticated
-#     def get(self):
-#         self.render("admin.html")
+class Helpers
+    @@S3_KEY='AKIAJNFVHNCXMSLWZOAA'
+    @@S3_SECRET='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'
+
+    def self.write_upload_to_disk(filename, file_data)
+        File.open(filename, "wb") { |f| f.write(file_data) }
+    end
+
+    def self.s3_upload(img_data, extension, uuid)
+        name = uuid + extension
+
+        connection = Fog::Storage.new(
+            :provider                 => 'AWS',
+            :aws_secret_access_key    => @@S3_SECRET,
+            :aws_access_key_id        => @@S3_KEY
+        )
+
+        directory = connection.directories.create(
+            :key    => "smm-albumart",
+            :public => true
+        )
+    
+        content_type = case extension
+        when ".gif"
+            "image/gif"
+        when ".png"
+            "image/png"
+        when ".jpeg" || ".jpg"
+            "image/jpeg"
+        else
+            ""
+        end
+
+        file = directory.files.create(
+            :key    => name,
+            :body   => img_data,
+            :content_type => content_type,
+            :public => true
+        )
+    
+        if extension == ".gif"
+            return "https://s3.amazonaws.com/"+bucket+"/"+name
+        else
+            return "http://trash.imgix.net/#{name}"
+        end
+    end
+end
 
 
 # ##########   /*     */   ##########
@@ -70,54 +86,31 @@ end
 #         else:
 #             self.redirect("/")
 
-# class AddHandler(BaseHandler):
-#     @tornado.web.authenticated
-#     def get(self):
-#         global admins
-#         if self.current_user['email'] in admins:
-#             self.render("add.html")
 
-#     @tornado.web.authenticated
-#     def post(self):
-#         global admins
-#         if self.current_user['email'] in admins:
-#             album = self.get_argument('album', None)
-#             print album
-#             catnum = self.get_argument('remove', None)
-#             if album:
-#                 temp = json.loads(album)
-#                 c = pymongo.Connection('localhost')
-#                 db = c.albums
-#                 albums = db.albums
-#                 old = albums.find_one({'catnum':str(temp['catnum'])})
-#                 if old:
-#                     old.update(json.loads(album))
-#                     albums.save(old)
-#                 else:
-#                     albums.insert( json.loads(album) )
-#                 self.write( json.dumps({'msg': 'success'}) )
-#             elif catnum:
-#                 c = pymongo.Connection('localhost')
-#                 db = c.albums
-#                 db.albums.remove({'catnum': catnum})
-#                 self.redirect("/")
-#         else:
-#             self.write( json.dumps({'msg': 'error'}) )
+get '/add' do
+    erb :add, :locals => {
+        :av => assets_version
+    }
+end
 
+post '/add' do
+    puts params[:album]
+end
 
-# ##########   /*     */   ##########
+post '/delete' do
 
+end
 
-# class UploadHandler(BaseHandler):
-#     @tornado.web.authenticated
-#     def post(self):
-#         if "X-File-Name" in self.request.headers:
-#             file_name   = self.request.headers['X-File-Name']
-#             print "receiving: " + file_name
-#             try:
-#                 f = open( './static/img/albums/'+file_name, 'w' )
-#                 f.write( self.request.body )
-#             except IOError:
-#                 print "IOError"
-#             else:
-#                 f.close()
+post '/uploadfile' do
+    extension = File.extname(request.env['HTTP_X_FILENAME']).downcase
+
+    if allowed_video_upload_formats.include? extension
+        uuid = UUIDTools::UUID.random_create.to_s
+
+        s3_url = Helpers.s3_upload( request.env["rack.input"].read, extension, uuid )
+
+        return { :result => "success", :msg => s3_url }.to_json
+    else
+        return { :result => "fail", :msg => "invalid file" }.to_json
+    end
+end
